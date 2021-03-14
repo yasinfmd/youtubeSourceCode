cccf3ef6715478622c205b787979a40b9025d0ca
yasinfmd <ysndlklc1234@gmail.com>
2021-03-14 16:56:26 +0300

    added custom v-model

---
+++ vuejs custom v_model/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<p>{{user}}</p>
+<custom-input v-model:firstName.bigger="user.name" v-model:age.reverse="user.age" />
+</template>
+
+<script>
+import CustomInput from './components/CustomInput.vue'
+export default {
+  components: { CustomInput },
+  data(){
+    return{
+      user:{
+        name:'',
+        age:''
+      }
+    }
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vuejs custom v_model/src/components/CustomInput.vue
@@ -0,0 +1,51 @@
+<template>
+<div>
+    <h2>Form</h2>
+    <div>
+<input :value='firstName' @input="onChangeFirstName($event)" />
+    </div>
+    <br/><br/>
+    <div>
+    
+<input  :value='age' @input="onChangeAge($event)" />
+</div>
+</div>
+    
+</template>
+
+<script>
+export default {
+    created(){
+        console.log(this.firstNameModifiers)
+        console.log(this.ageModifiers)
+    },
+    methods:{
+           onChangeAge(e){
+                let ageValue=e.target.value;
+                if(this.ageModifiers){
+                    ageValue=ageValue.split('').reverse().join('')
+                }
+                this.$emit('update:age',ageValue)
+           },
+            onChangeFirstName(e){
+                let firstNameValue=e.target.value
+                if(this.firstNameModifiers){
+                     firstNameValue=firstNameValue.toUpperCase();   
+                }
+                this.$emit('update:firstName',firstNameValue)
+            }
+    },
+    props:{
+        firstName:String,
+        age:String,
+        firstNameModifiers:{
+            default:()=>{}
+        },
+        ageModifiers:{
+            
+        }
+    },
+    
+    emits:['update:firstName','update:age']
+}
+</script>
