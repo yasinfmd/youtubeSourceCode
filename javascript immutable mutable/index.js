e49dc62184c5d208d1ec9860cce17975f7e98374
yasinfmd <ysndlklc1234@gmail.com>
2021-03-19 17:56:37 +0300

    added js immutable and mutable

---
+++ javascript immutable mutable/index.js
@@ -0,0 +1,44 @@
+var Array1 = [1, 2, 3, 4, 5, 6]
+
+var Array2 = Array1;
+console.log('Array1', Array1)
+console.log('Array2', Array2)
+Array1.push(99)
+console.log('Array1', Array1)
+console.log('Array2', Array2)
+Array2.pop()
+console.log('Array1', Array1)
+console.log('Array2', Array2)
+Array2.shift();
+console.log('Array1', Array1)
+console.log('Array2', Array2)
+
+
+
+
+
+
+
+/*
+    kisi1 nesnesi Ramde ABC
+    kisi2 nesnesine kisi1  ABC
+    kisi1 değişim
+    kisi2 ABC
+    kisi1 ABC
+
+*/
+
+/*
+sayi 1 Ramde Abc123
+sayi 2 Ramde Abc1234
+
+sayi 1 Ramde Abc12345
+
+Sayı 2 Ramde Abc1234567
+
+
+Sayı 1 Ramde Abc12321378asjdh
+
+
+
+*/
