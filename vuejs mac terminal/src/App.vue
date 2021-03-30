fc45a6880288e5ef3a2afdf53b8d81484aae4dc2
Yasin Efem  Dalkılıç <yefem.dalkilic@gmail.com>
2021-03-30 18:10:00 +0300

    vuejs mac terminal view

---
+++ vuejs mac terminal/src/App.vue
@@ -0,0 +1,68 @@
+<template>
+<terminal :logData="logData" />
+</template>
+
+<script>
+import Terminal from './components/Terminal.vue'
+export default {
+   name: "App",
+   data(){
+     return{
+        logData:[
+          {
+            text:`
+          Corrupti voluptatibus adipisci debitis officia. Provident facilis cum reprehenderit, illo, distinctio quas sapiente voluptas, asperiores officiis quam modi est aspernatur sint eius fuga ullam corrupti similique cupiditate. Ullam, sapiente fugiat.
+            `
+          },
+            {
+            text:`
+            Facere illum inventore, corrupti fugiat tempore quidem repellat! Non consectetur voluptate nulla dignissimos quas, vitae numquam voluptatem fugiat quos adipisci, commodi quisquam sit earum atque inventore? Consectetur enim laudantium suscipit.
+            `
+          },
+          {
+            text:`
+              Tenetur, asperiores. Pariatur, a optio. Error eligendi porro alias molestias odit iure at autem illo deserunt explicabo! Vitae illum, maiores, aut iure consequatur magnam dolorum laudantium officia illo quod doloremque.
+            `
+          },
+            {
+            text:`
+            Iure necessitatibus odit tempore id odio rerum inventore error quas, modi, neque corporis ad adipisci excepturi debitis? Suscipit, mollitia! Nemo, saepe reprehenderit ipsa molestiae nam voluptatem rem! Atque, sit eligendi!
+            `
+          },
+            {
+            text:`
+              Odit maiores dolores commodi fuga exercitationem aliquid ea, dicta sapiente non quidem! Doloribus aliquam et quisquam iusto incidunt voluptates? Reprehenderit, debitis totam. Excepturi debitis neque minus odio! Placeat, rem fugit?
+            `
+          },
+            {
+            text:`
+              Voluptatum ducimus aperiam laboriosam temporibus magni sequi id eveniet, ipsum reiciendis fugiat corporis earum necessitatibus nisi harum exercitationem voluptatibus vitae omnis animi aliquam! Delectus nobis aliquam veritatis sapiente tempore laudantium.
+              Tenetur, asperiores. Pariatur, a optio. Error eligendi porro alias molestias odit iure at autem illo deserunt explicabo! Vitae illum, maiores, aut iure consequatur magnam dolorum laudantium officia illo quod doloremque.
+            `
+          }
+        ]
+     }
+   },
+   mounted(){
+     const _this=this;
+      setTimeout(() => {
+        _this.logData=[..._this.logData,{text:`Merhaba Yeni Log ${new Date().toLocaleString()}`,isNew:true}]
+      }, 1000);
+      console.log(this.logData)
+   },
+   components:{
+      Terminal
+   }
+}
+</script>
+
+<style>
+#app {
+   font-family: Avenir, Helvetica, Arial, sans-serif;
+   -webkit-font-smoothing: antialiased;
+   -moz-osx-font-smoothing: grayscale;
+   text-align: center;
+   color: #2c3e50;
+   margin-top: 60px;
+}
+</style>
---
+++ vuejs mac terminal/src/components/Terminal.vue
@@ -0,0 +1,146 @@
+<template>
+    <div class="terminalWrapper">
+    <div class="terminalWindow">
+    <i class="close"></i>
+    <i class="resize"></i>
+    <i class="minimize"></i>
+    <p>Başlık</p>
+    </div>
+    <div class="terminalBody">
+        <div class="terminal">
+        <p v-for="(item,index) in logData" :key='index' class="log" :class="(item.isNew && item.isNew ===true) ? 'animationLog':'' ">
+        {{item.text}}
+        </p>
+        
+            <div class="terminalEndLine"> <span >_</span> </div>
+        </div>
+    </div>
+    </div>    
+</template>
+
+<script>
+export default {
+    props:['logData']
+    
+}
+</script>
+
+<style scoped>
+.terminalWrapper{
+
+    display: flex;
+    background: #262626;
+    flex-direction: column;
+    box-sizing: border-box;
+    border-radius: 6px;
+    position: relative;
+}
+.terminalWindow{
+    height: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    margin-left: 8px;
+    border-bottom: 1px solid #262626;
+}
+.terminalWindow i {
+    width: 1em;
+    height: 1em;
+    font-size: 10px;
+    border-radius: 9999px;
+    margin-right: 6px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+}
+.terminalWindow p {
+    font-weight: 600;
+    font-size: 12px;
+    color: #d9d9d9;
+}
+.close{
+    background: #f5222d;
+}
+.resize{
+    background: #ffc53d;
+    
+}
+.minimize{
+    background: #52c41a;
+    
+}
+
+.terminalBody{
+    display: flex;
+    width: 100%;
+    background: #141410;
+    box-sizing: border-box;
+    height: 400px;
+}
+.terminal{
+    box-sizing: border-box;
+    padding: 12px;
+    overflow-x: hidden;
+    overflow-y: scroll;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    width: 100%;
+}
+.log{
+    text-align: left;
+    color: #8ab4f8;
+    width: 100%;
+    font-size: 11px;
+
+}
+.animationLog{
+     animation: newLog 1000ms;
+}
+.terminalEndLine{
+    background: currentColor;
+    width: 4px;
+    height: 16px;
+    margin-top: 6px;
+    animation: terminalAnimation 700ms infinite;
+}
+@keyframes newLog {
+     0%{
+         opacity: 0;
+     }
+        50%{
+         opacity: 1;
+     }
+         100%{
+         opacity: 0;
+     }
+}
+
+@keyframes terminalAnimation {
+     0%{
+         opacity: 0;
+     }
+        50%{
+         opacity: 1;
+     }
+         100%{
+         opacity: 0;
+     }
+}
+
+.terminalEndLine span{
+    opacity: 0;
+}
+::-webkit-scrollbar{
+    width:4px;
+}
+::-webkit-scrollbar-track{
+    background: #141410;
+    border-radius: 20px;
+}
+::-webkit-scrollbar-thumb{
+    background: #262626;
+}
+</style>
