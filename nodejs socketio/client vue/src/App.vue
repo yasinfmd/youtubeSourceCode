d8e1699116077abdef208a1a6440b93246a29b84
Yasin Efem  Dalkılıç <yefem.dalkilic@gmail.com>
2021-04-15 15:36:03 +0300

    nodejs socket

---
+++ nodejs socketio/client vue/src/App.vue
@@ -0,0 +1,74 @@
+<template>
+   <div id="app">
+   <h2>Benim Socket Id : {{socketId}}</h2>
+   <button @click="onSendMsg()">Servera Socket Üzerinden Merhaba De :) </button>
+
+   <div>
+   <h4>Online Kullanıcılar {{onlineUsers.length}}</h4>
+   <ul v-for="item in onlineUsers" :key="item">
+   <li>{{item}} <button @click="selectedUser = item">Konuş</button></li>
+   </ul>
+   </div>
+   <div>
+    Konusulacak Kişi {{selectedUser}} <button @click="onSendHello()">Selam Çak :)</button>
+   </div>
+   </div>
+</template>
+
+<script>
+import {io} from 'socket.io-client'
+export default {
+   name: "App",
+   data(){
+     return{
+       socket:{},
+       socketId:'',
+       onlineUsers:[],
+       selectedUser:''
+     }
+   },
+   created(){
+     this.socket=io('http://localhost:3000/')
+     console.log('socket',this.socket)
+   },
+   methods:{
+     onSendHello(){
+      this.socket.emit('chat',{userid:this.selectedUser,meid:this.socketId, msg:'Merhaba Arkadaş'})
+     },
+    onSendMsg(){
+      this.socket.emit('clientToServer',{msg:'Merhaba Dünya'})
+    }
+   },
+   mounted(){
+      this.socket.on('customConnection',(socketId)=>{
+        this.socketId=socketId;
+        console.log(`Serverdan Sana Gelen Socket ID : ${socketId}`)
+      })
+      this.socket.on('welcome',(msg)=>{
+        console.log('Serverdan Tümüne Gönderilen Mesaj'+ ' ' + msg)
+      })
+
+      this.socket.on('otherwelcom',(msg)=>{
+        console.log('Msg',msg)
+      })
+      this.socket.on('onlineUsers',(usersArray)=>{
+        this.onlineUsers=usersArray;
+      })
+        this.socket.on('chat',(message)=>{
+          console.log('mesaj',message)
+      })
+
+   }
+};
+</script>
+
+<style>
+#app {
+   font-family: Avenir, Helvetica, Arial, sans-serif;
+   -webkit-font-smoothing: antialiased;
+   -moz-osx-font-smoothing: grayscale;
+   text-align: center;
+   color: #2c3e50;
+   margin-top: 60px;
+}
+</style>
---
+++ nodejs socketio/server/app.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const app = express();
+const http = require('http')
+const server = http.createServer(app)
+const io = require('socket.io')(server, {
+    cors: {
+        origin: 'http://localhost:8080'
+    }
+})
+
+let users = new Set();
+
+io.on('connection', (socket) => {
+    console.log('connection' + '  ' + socket.id)
+    socket.on('disconnect', () => {
+        console.log('Kullanıcı gidiyorrr' + ' ' + socket.id)
+        users.delete(socket.id)
+        // console.log('users', users)
+    })
+    // o an bağlanan client için socket bilgisini gönderdik
+    socket.emit('customConnection', socket.id)
+    //bağlı olan tüm clientlara mesaj göndermek
+    //io.emit('welcome', '15 Nisan 2021 Hoşgeldiniz')
+
+    // bağlı olan client haric tüm clientlara mesaj göndermek
+    socket.broadcast.emit('otherwelcom', 'Ben Hariç Herkese')
+
+
+    socket.on('chat', (data) => {
+        io.to(data.userid).emit('chat', { mesaj: data.msg, kimden: data.meid })
+    })
+    socket.on('clientToServer', (json) => {
+        // ne yapmak istiyosam
+        console.log('socketId', socket.id)
+        console.log('istemciden geliyor', json)
+    })
+    const connectedCl = io.sockets.adapter.rooms
+    connectedCl.forEach((value, key) => {
+        users.add(key)
+    })
+    users.forEach((socketId) => {
+        console.log('socketId', socketId)
+        /*
+            1 ali
+            2 veli
+            3 ayşe
+
+        */
+        io.to(socketId).emit('onlineUsers', findUsers(socketId))
+    })
+    console.log('users', users)
+    //console.log('connectedCl', connectedCl)
+
+
+})
+
+const findUsers = (id) => {
+    const arr = [...users].filter((item) => {
+        return item !== id
+    })
+    console.log('array', arr)
+    return arr
+}
+
+
+
+server.listen(3000, () => {
+    console.log('Server Listening at Port ' + 3000)
+})
