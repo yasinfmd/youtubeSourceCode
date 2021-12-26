b7039d49b64fa5e7681baa0c64f9cb0e99d02acd
Yasin Efem  Dalkılıç <yefem.dalkilic@gmail.com>
2021-12-26 15:14:19 +0300

    angular component

--- angular component/src/app/app.component.ts
+++ angular component/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-first-project';
+
+  clicked(str:string){
+    alert(str)
+  }
 }
--- angular component/src/app/components/hello-world/hello-world.component.ts
+++ angular component/src/app/components/hello-world/hello-world.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-hello-world',
+  // selector:'.appHelloWorld',
+  // selector:'[appHelloWorld]',
   templateUrl: './hello-world.component.html',
   styleUrls: ['./hello-world.component.css']
 })
 export class HelloWorldComponent {
-    componentName:string="Hello World Componentiyim"
+  @Input() messageName: string = ""
+  @Input("parentName") customMessage: string = ""
+
+  @Output() clickEventMessage = new EventEmitter<string>();
+
+  @Output("customClick") clickOtherEventMessage=new EventEmitter<string>();
+
+
+  sendParentOnClick(){
+    this.clickEventMessage.emit("Butona Tıklandı Child Componentden")
+    this.clickOtherEventMessage.emit("İsimlendirilmiş Tıklama")
+  }
+
+  componentName: string = "Hello World Componentiyim"
 }
