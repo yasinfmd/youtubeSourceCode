ef3722934e17cd38a2558f95fa51a0b4085b09fb
Yasin Efem  Dalkılıç <yefem.dalkilic@gmail.com>
2021-12-26 12:26:16 +0300

    angular create component

---
+++ angular component/src/app/components/hello-world/hello-world.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-world',
+  templateUrl: './hello-world.component.html',
+  styleUrls: ['./hello-world.component.css']
+})
+export class HelloWorldComponent {
+    componentName:string="Hello World Componentiyim"
+}
