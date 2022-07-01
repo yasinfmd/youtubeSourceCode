79c7126ac493a42ba7dd3e2e7d60ef642e33a502
Yasin Efem  Dalkılıç <yefem.dalkilic@gmail.com>
2022-07-01 19:50:08 +0300

    svelte reaktif and props

---
+++ svelte reaktif &  props/src/MySvelteComponent.svelte
@@ -0,0 +1,28 @@
+
+<script>
+    import {createEventDispatcher} from 'svelte'
+ export let title;
+ export let body;
+ export let content;
+ export  let arr;
+
+ export let onPress=()=>null
+
+    const dispatch=createEventDispatcher()
+
+</script>
+
+<div>
+    <div>{title}</div>
+    <div>{body}</div>
+    <div>{content}</div>
+    <div>
+        {arr}
+    </div>
+
+    <button on:click={()=>{
+        dispatch('custom-click',{id:100})
+    }}>Tıklaaaa 2</button>
+
+    <button on:click={onPress}>Tıklaaaa</button>
+</div>
