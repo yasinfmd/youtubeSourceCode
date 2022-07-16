3af4ffb9f964cc28554405a9c83729dbb9b55af2
Yasin Efem  Dalkılıç <yefem.dalkilic@gmail.com>
2022-07-16 21:57:35 +0300

    svelte cross component data share

---
+++ svelte croos component data share/src/App.svelte
@@ -0,0 +1,59 @@
+<script>
+  import HelloWorld , {test,showAlert} from "./HelloWorld.svelte";
+  let myVal=test
+</script>
+<main>
+<div>Hello</div>
+  <button on:click={()=>{
+    myVal="aaaa"
+  }}>Parent Component Değiştir</button>
+  {myVal}
+  <HelloWorld />
+  <button on:click={()=>{
+        showAlert('Parent')
+    }}>Mesaj Göster</button>
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+
+  img {
+    height: 16rem;
+    width: 16rem;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4rem;
+    font-weight: 100;
+    line-height: 1.1;
+    margin: 2rem auto;
+    max-width: 14rem;
+  }
+
+  p {
+    max-width: 14rem;
+    margin: 1rem auto;
+    line-height: 1.35;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      max-width: none;
+    }
+
+    p {
+      max-width: none;
+    }
+  }
+</style>
---
+++ svelte croos component data share/src/HelloWorld.svelte
@@ -0,0 +1,28 @@
+<script context="module">
+   export let test="Test Yazısı"
+   export const showAlert=(msg)=>{
+       alert(msg)
+   }
+</script>
+<script>
+    let ad="Yasin"
+</script>
+
+<div>
+    {ad}
+    {test}
+    <button on:click={()=>{
+        ad="Yasin 2"
+        test="Test Yazısı 2"
+    }}>Değiştir</button>
+
+    <button on:click={()=>{
+        showAlert('Child')
+    }}>Mesaj Göster</button>
+</div>
+
+<style>
+    div{
+        color: #ff3e00;
+    }
+</style>
