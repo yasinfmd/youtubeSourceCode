c8896b7568b0d35f407b52e43b14b4c2daa83d27
Yasin Efem  Dalkılıç <yefem.dalkilic@gmail.com>
2022-07-17 11:23:09 +0300

    svelte style class bind

---
+++ svelte class style bind/src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+  let myclassName="custom-btn"
+  let number=1
+
+  let myStyle={
+    color:'blue',
+    font:'bold'
+
+  }
+</script>
+<main>
+<div class={`custom-div`}>Hello</div>
+  <div class:custom-btn={number%2===0}
+       class:custom-div={number%2===0}
+  >Hello</div>
+
+  <button on:click={()=>{
+    number++
+  }}>Değiştir</button>
+
+  <div style={`color:${myStyle.color};font-weight:${myStyle.font}`}>Test</div>
+
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  .custom-div{
+    background: darkred;
+  }
+  .custom-btn{
+    border: 2px solid darkblue;
+  }
+</style>
