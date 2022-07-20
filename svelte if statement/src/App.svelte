db11a1c7f0209c580fc63bf74ce2600590ffd4e2
Yasin Efem  Dalkılıç <yefem.dalkilic@gmail.com>
2022-07-20 19:33:56 +0300

    svelte if statement

---
+++ svelte if statement/src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+  let number=1
+  let isExist=false
+</script>
+<main>
+  {#if isExist}
+    <h1>Kayıt Var</h1>
+    {:else}
+    <h1>Kayıt Yok</h1>
+    {/if}
+    {#if number===5}
+    <h1>Sayı 5</h1>
+    {:else if number===10}
+    <h1>Sayı 10</h1>
+    {:else }
+    <h1>Sayı küçük</h1>
+    {/if}
+  {number}
+
+  <button on:click={()=>{number=5}}>Sayıyı 5 yap</button>
+  <button on:click={()=>{number=10}}>Sayıyı 10 yap</button>
+  <button on:click={()=>{number=2}}>Sayıyı 2 yap</button>
+
+  <button on:click={()=>{isExist=!isExist}}>Tıkla ve Değiştir</button>
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+
+</style>
