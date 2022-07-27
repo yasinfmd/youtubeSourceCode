53eefe490286e29f085c633d0538e320e2f5225f
Yasin Efem  Dalkılıç <yefem.dalkilic@gmail.com>
2022-07-27 19:05:29 +0300

    svelte lifecycles

---
+++ svelte lifecycles/src/App.svelte
@@ -0,0 +1,40 @@
+<script>
+    import {onMount,beforeUpdate,afterUpdate} from 'svelte'
+    import HelloWorld from "./HelloWorld.svelte";
+    onMount(()=>{
+        //http requests
+        console.log('Bağlandı')
+        console.log(myName)
+
+
+    })
+    beforeUpdate(()=>{
+        console.log('Beforeupdate')
+        console.log(myName)
+    })
+    afterUpdate(()=>{
+        console.log('after update')
+        console.log(myName)
+
+    })
+    let myName='Yasin'
+    let show=true
+</script>
+<main>
+    {myName}
+    <button on:click={()=>{
+        show=!show
+        myName=Math.random().toString(36)}}>Değiştir</button>
+    {#if show}
+    <HelloWorld />
+        {/if}
+</main>
+
+<style>
+    :root {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+        Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+
+
+</style>
---
+++ svelte lifecycles/src/HelloWorld.svelte
@@ -0,0 +1,10 @@
+<script>
+    import {onDestroy, onMount} from 'svelte'
+    onMount(()=>{
+        console.log('Yaşıyorumm')
+    })
+    onDestroy(()=>{
+        console.log('Ölüyorummm')
+    })
+</script>
+<h1>Hello World</h1>
