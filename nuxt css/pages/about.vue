3cd8b87b203a7dc1c18bf31db93a34a44759c8d2
Yasin Efem  Dalkılıç <yefem.dalkilic@gmail.com>
2022-08-21 14:54:18 +0300

    nuxt css

---
+++ nuxt css/pages/about.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="mydiv">
+    About
+    <NuxtLink to="/">Home</NuxtLink>
+    <button class="myclass" @click="$router.push('/')">Home</button>
+  </div>
+</template>
+
+<script>
+export default {
+  layout:'custom',
+  name: "about"
+}
+</script>
+
+<style scoped>
+  .mydiv{
+    padding: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid #ccc;
+  }
+</style>
