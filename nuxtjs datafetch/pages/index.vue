693ae1b7742260562b896b7fc396c4eab777eb2a
Yasin Efem  Dalkılıç <yefem.dalkilic@gmail.com>
2022-08-24 19:06:58 +0300

    nuxtjs data fetch

---
+++ nuxtjs datafetch/pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <Tutorial/>
+    <ul>
+      <li v-for="item in data">{{item}}</li>
+    </ul>
+    <NuxtLink to="/about">About</NuxtLink>
+    {{ad}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data(){
+    return {
+      ad:'Yasin'
+    }
+  },
+  asyncData(context){
+    //callback
+    //callback(null,{data:[1,2,3]})
+    return new Promise((resolve => {
+      resolve({data:[1,2,3,4,5]})
+    })).then((data)=>{
+      return data
+    })
+  }
+
+}
+</script>
---
+++ nuxtjs datafetch/pages/items/index.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  Items Page
+</div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
