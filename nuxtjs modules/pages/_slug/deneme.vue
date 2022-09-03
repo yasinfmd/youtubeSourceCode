8f5de899999a53a9c3547551c8e41abf1c54091f
Yasin Efem  Dalkılıç <yefem.dalkilic@gmail.com>
2022-09-03 15:37:00 +0300

    nuxtjs modules

---
+++ nuxtjs modules/pages/_slug/deneme.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Deneme Sayfası  {{$route.params}}</div>
+</template>
+
+<script>
+export default {
+  name: "deneme",
+  created() {
+    console.log(this.$route.params)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ nuxtjs modules/pages/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <NuxtTutorial />
+<!--    <ul>
+      <li v-for="item in data">{{item}}</li>
+    </ul>-->
+    <NuxtLink to="/about">About</NuxtLink>
+    {{ad}}
+    Sayaç {{$store.state.counter}}
+    İsim {{publishedBooksMessage}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  computed: {
+    // a computed getter
+    publishedBooksMessage() {
+      console.log(this.$store.getters["users/getName"])
+      // `this` points to the component instance
+      return this.$store.state.users.name
+    }
+  },
+  methods:{
+    async fetchSomething() {
+      const ip = await this.$axios.$get('http://icanhazip.com')
+      alert(ip)
+    }
+  },
+  created() {
+    this.fetchSomething()
+    console.log(process.env.name)
+      console.log(this.$store.state.counter)
+    setTimeout(()=>{
+      //this.$store.dispatch('users/changeName','Efem')
+      this.$store.dispatch('setCount',30)
+    },2000)
+  },
+  async fetch(ctx){
+    //istek
+   const result= await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    const res=await  result.json()
+    console.log('res',res.title)
+    ctx.store.dispatch('users/changeName',res.title)
+
+/*    setTimeout(()=>{
+
+    },3000)*/
+  },
+  data(){
+    return {
+      ad:'Yasin'
+    }
+
+  },
+/*  asyncData(context){
+    //callback
+    //callback(null,{data:[1,2,3]})
+    return new Promise((resolve => {
+      resolve({data:[1,2,3,4,5]})
+    })).then((data)=>{
+      return data
+    })
+  }*/
+
+}
+</script>
