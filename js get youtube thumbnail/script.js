2f0ff174594b1d19245d56778ad1291387fddc4b
Yasin Efem  Dalkılıç <yefem.dalkilic@gmail.com>
2022-10-20 11:27:18 +0300

    js youtube thumbnail

---
+++ js get youtube thumbnail/script.js
@@ -0,0 +1,9 @@
+
+const mybtn=document.getElementById('mybtn')
+
+const myimg=document.getElementById('myimg')
+
+
+mybtn.addEventListener('click',()=>{
+    myimg.src="https://img.youtube.com/vi/ODVzN_F22K4/maxresdefault.jpg"
+})
