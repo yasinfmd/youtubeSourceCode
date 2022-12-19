eaa4c96995ac6719108a6f68309a131884e4763e
Yasin Efem  Dalkılıç <yefem.dalkilic@gmail.com>
2022-12-19 22:34:34 +0300

    react css win11 template

---
+++ react win11 template css/src/App.js
@@ -0,0 +1,13 @@
+import { useEffect } from 'react';
+import './App.css';
+import BottomBar from './BottomBar';
+
+function App() {
+  return (
+    <div>
+      <BottomBar />
+    </div>
+  );
+}
+
+export default App;
