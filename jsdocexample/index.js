90b22ef0adaf011e8913b23900abdf0f49747c4c
Yasin Efem  Dalkılıç <yefem.dalkilic@gmail.com>
2022-12-22 20:44:25 +0300

    js jsdocs example

---
+++ jsdocexample/index.js
@@ -0,0 +1,34 @@
+/**
+ * Bu fonksiyon bir işlem yapar
+ * @param {string} title - Başlık bir işlemde kullanmak için gerekli
+ * @param {int} age - Yaş kullanıcı için gerekli
+ * @return {Promise<string>} - Bu bir string verir
+ * @fires Hurl#snowball
+ * @async
+ */
+const myFunction=(title,age)=>{}
+
+myFunction()
+
+/**
+ * Bir sınıf oluşturur
+ * @class
+ */
+function MyClass(){
+
+}
+new MyClass()
+
+/** @type {(string|Array.)} */
+var foo;
+/** @type {number} */
+var bar = 1;
+
+
+/**
+ * Interface for classes that represent a color.
+ *
+ * @interface
+ */
+function Color() {}
+
