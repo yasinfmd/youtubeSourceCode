68b1a1737450db5bd53b71f29760f4fe1944e290
Yasin Efem  Dalkılıç <yefem.dalkilic@gmail.com>
2023-01-30 22:00:44 +0300

    vuejs recursive componenets

---
+++ vuejs nested componenets/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <NestedComp :data="myData" />
+</template>
+
+<script>
+import NestedComp from './components/NestedComp.vue';
+
+
+export default {
+    name: "App",
+    data() {
+        return {
+            myData: [
+                {
+                    name: "Başlık1",
+                    children: [
+                        {
+                            name: "Başlık 1.1",
+                            children: []
+                        },
+                        {
+                            name: "Başlık 1.2",
+                            children: []
+                        }
+                    ]
+                },
+                {
+                    name: "Başlık 2",
+                    children: [
+                        {
+                            name: "Başlık 2.1",
+                            children: []
+                        },
+                        {
+                            name: "Başlık 2.2",
+                            children: []
+                        },
+                        {
+                            name: "Başlık 2.3",
+                            children: [
+                              {
+                                name:'Başlık 2.3 -1',
+                                children:[]
+                              }
+                            ]
+                        }
+                    ]
+                },
+            ]
+        };
+    },
+    components: { NestedComp }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vuejs nested componenets/src/components/NestedComp.vue
@@ -0,0 +1,18 @@
+<template>
+
+    <ul>
+    <li v-for="item in data">
+        {{ item.name }}
+    <NestedComp v-if="item.children" :data="item.children" />
+    </li>
+    </ul>
+</template>
+
+
+<script>
+
+export default {
+    name:"NestedComp",
+    props:['data']
+}
+</script>
