61829b04e8a768f7a99f5335e15cfb71cf18a7bc
Yasin Efem  Dalkılıç <yefem.dalkilic@gmail.com>
2023-02-05 13:55:36 +0300

    flutter generate qrcode

---
+++ flutter qrcode generate/android/app/src/main/kotlin/com/example/generate_qr_code/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.generate_qr_code
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter qrcode generate/build/app/generated/source/buildConfig/debug/com/example/generate_qr_code/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.generate_qr_code;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.generate_qr_code";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
