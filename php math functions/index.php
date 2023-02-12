ea0b279c3e059db6142ba051be119573263e28f8
Yasin Efem  Dalkılıç <yefem.dalkilic@gmail.com>
2023-02-12 20:50:18 +0300

    php math functions

---
+++ php math functions/index.php
@@ -0,0 +1,59 @@
+<?php
+echo(abs(-1));
+echo "\n";
+
+echo bindec("11111001");
+echo "\n";
+echo decbin("249");
+echo "\n";
+echo ceil(5.1);
+echo "\n";
+echo cos(0);
+echo "\n";
+echo sin(0);
+echo "\n";
+
+echo dechex(70);
+echo "\n";
+echo hexdec(46);
+echo "\n";
+echo deg2rad(360);
+echo "\n";
+echo rad2deg(6.2831853071796);
+echo "\n";
+echo floor(4.2);
+echo "\n";
+echo fmod(20,3);
+echo "\n";
+echo getrandmax();
+
+echo "\n";
+echo max(array(42,55,7887,254,7546));
+
+echo "\n";
+echo min(array(42,55,7887,254,7546));
+echo "\n";
+
+echo mt_rand();
+echo "\n";
+echo mt_rand(10,50);
+echo "\n";
+echo pi();
+
+echo "\n";
+echo pow(2,4);
+
+
+echo "\n";
+echo round(4.2);
+echo "\n";
+echo round(4.6);
+
+echo "\n";
+echo sqrt(9);
+
+echo "\n";
+
+echo sqrt(45);
+
+?>
