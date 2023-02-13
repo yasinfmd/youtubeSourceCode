7a030b9bc23304543e863f71134ed277b9176363
Yasin Efem  Dalkılıç <yefem.dalkilic@gmail.com>
2023-02-13 21:21:08 +0300

    astro vue integration

---
+++ astro vue integration/astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
+import vue from "@astrojs/vue";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  integrations: [react(), vue()]
+});
---
+++ astro vue integration/src/components/MyVueComp.vue
@@ -0,0 +1,31 @@
+<template>
+    <button @click="changeNameHandler">İsim Değiştir</button>
+    <input type="text" v-model="inputVal">
+    <p>{{ inputVal }}</p>
+    Merhaba Ben Vuejs Bileşeniyimmmm
+    <h1>{{ myName }}</h1>
+</template>
+
+
+<script setup>
+import { ref } from 'vue';
+
+const inputVal=ref("")
+const myName=ref("Yasin")
+
+const changeNameHandler=()=>{
+    myName.value=Math.random()
+}
+</script>
+<style scoped>
+button{
+    height: 40px;
+    display: flex;
+    padding: 8px;
+    background: #000;
+    color: #fff;
+    border-radius: 8px;
+    border: none;
+    outline: none;
+}
+</style>
