26b268365ccea5ccc6758f047682d824fd519457
Yasin Efem  Dalkılıç <yefem.dalkilic@gmail.com>
2023-03-11 18:12:52 +0300

    flutter ss

---
+++ flutter screen shot/android/app/src/main/kotlin/com/example/flutter_ss/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_ss
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter screen shot/lib/main.dart
@@ -0,0 +1,97 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:screenshot/screenshot.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  late Uint8List image;
+  bool show=false;
+  ScreenshotController controller=ScreenshotController();
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Screenshot(child: Text("Merhaba dünyaaaaa"), controller: controller),
+            Container(color: Colors.red,
+            width: double.infinity,
+              height: 400,
+              child: show ? Image.memory(image):SizedBox.shrink(),
+            ),
+
+            TextButton(onPressed: (){
+              controller.capture().then((value){
+                print(value);
+                var str2=base64Encode(value!);
+                String header = "data:image/png;base64,";
+                var str= base64UrlEncode(value!);
+                print(header+str);
+                print(header+str2);
+
+                setState(() {
+                  image=value!;
+                  show=!show;
+                });
+              });
+            }, child: Text("Ekran görüntüsü al")),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
