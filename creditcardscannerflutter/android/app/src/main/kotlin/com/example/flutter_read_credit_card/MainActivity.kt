119ab655d7c852fac2c990df6038394d82a1703a
Yasin Efem  Dalkılıç <yefem.dalkilic@gmail.com>
2023-03-19 10:12:11 +0300

    flutter credit card scanner

---
+++ creditcardscannerflutter/android/app/src/main/kotlin/com/example/flutter_read_credit_card/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_read_credit_card
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ creditcardscannerflutter/build/card_scanner/generated/source/buildConfig/debug/com/nateshmbhat/card_scanner/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.nateshmbhat.card_scanner;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.nateshmbhat.card_scanner";
+  public static final String BUILD_TYPE = "debug";
+}
