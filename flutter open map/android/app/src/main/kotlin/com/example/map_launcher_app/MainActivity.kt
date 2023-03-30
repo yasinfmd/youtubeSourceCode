c6372c84feeeaf0ca84681664bd07e6a82416540
Yasin Efem  Dalkılıç <yefem.dalkilic@gmail.com>
2023-03-30 19:44:24 +0300

    flutter open map

---
+++ flutter open map/android/app/src/main/kotlin/com/example/map_launcher_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.map_launcher_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter open map/lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:map_launcher/map_launcher.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() async{
+
+    final availableMaps=await MapLauncher.installedMaps;
+    availableMaps.forEach((element) {
+      print(element.icon);
+      print(element.mapName);
+      print(element.mapType);
+    });
+    await availableMaps.first.showMarker(coords: Coords(39.925533, 32.866287), title: "Başlık",description: "Açıklama",zoom:50 );
+  //  MapLauncher.showDirections(mapType: mapType, destination: destination)
+    // await MapLauncher.isMapAvailable(MapType.google);
+    print(availableMaps);
+    setState(() {
+
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
