a8d07d35a72980f88e4e8e0246cef236b60a763c
Yasin Efem  Dalkılıç <yefem.dalkilic@gmail.com>
2023-04-11 21:48:38 +0300

    flutter android watch app

---
+++ flutter wearableapp/android/app/src/main/kotlin/com/example/pinch_to_zoom/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pinch_to_zoom
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter wearableapp/lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:wear/wear.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body:  WatchShape(
+            builder: (BuildContext context, WearShape shape, Widget? child) {
+              return
+                  ListView(
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    padding: const EdgeInsets.all(8),
+                    children: <Widget>[
+                      Container(
+                        height: 50,
+                        color: Colors.amber[600],
+                        child: const Center(child: Text('Entry A')),
+                      ),
+                      Container(
+                        height: 50,
+                        color: Colors.amber[600],
+                        child: const Center(child: Text('Entry A')),
+                      ),
+                      Container(
+                        height: 50,
+                        color: Colors.amber[600],
+                        child: const Center(child: Text('Entry A')),
+                      ),
+                      Container(
+                        height: 50,
+                        color: Colors.amber[600],
+                        child: const Center(child: Text('Entry A')),
+                      ),
+                      Container(
+                        height: 50,
+                        color: Colors.amber[600],
+                        child: const Center(child: Text('Entry A')),
+                      ),
+                      Container(
+                        height: 50,
+                        color: Colors.amber[500],
+                        child: const Center(child: Text('Entry B')),
+                      ),
+                      Container(
+                        height: 50,
+                        color: Colors.amber[100],
+                        child: const Center(child: Text('Entry C')),
+                      ),
+                    ],
+              );
+            },
+        ),
+      ),
+    );
+  }
+}
