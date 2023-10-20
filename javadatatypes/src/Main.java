1582e0b18500e0eb6cb1488f666d6eb4cdebdc85
Yasin Efem  Dalkılıç <yefem.dalkilic@gmail.com>
2023-10-20 20:53:49 +0300

    java datatypes

---
+++ javadatatypes/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int yas=30;
+        System.out.println(yas);
+
+        yas=50;
+        System.out.println(yas);
+
+        byte birthday=127;
+        long yas2=3;
+        double price=12.2;
+        boolean isUserActive=true;
+        char myChar='Y';
+        String myName="Yasin Dalkılıç";
+        double myDouble=100.5;
+        int converToInt=  (int)myDouble;
+        System.out.println(converToInt);
+    }
+}
