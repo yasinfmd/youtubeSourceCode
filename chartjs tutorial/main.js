7e13a03376d031c62807050e5d084fbdf187340d
Yasin Efem  Dalkılıç <yefem.dalkilic@gmail.com>
2024-04-23 21:15:33 +0300

    chartjstutorial1

---
+++ chartjs tutorial/main.js
@@ -0,0 +1,24 @@
+import './style.css'
+const ctx = document.getElementById('myChart');
+
+import Chart from 'chart.js/auto'
+
+
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
